067f5f78166c08212752c7f8df5bdfc535ae5086
Sayedul Karim <sayedulkrm@gmail.com>
2023-07-21 18:19:04 +0600

    Added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import user from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config({
     path: "./config/config.env",
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/v1", user);
 
